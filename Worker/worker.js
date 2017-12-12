c2d5b31c460a27a6e1aa804ca54dbd77bb57824a
spach10 <seth.pacheco@gmail.com>
2017-12-12 11:28:25 -0700

    remove python shell stuff

--- Worker/worker.js
+++ Worker/worker.js
@@ -1,6 +1,5 @@
 var amqp = require('amqplib/callback_api');
 var DBFunctions = require('../DBFunctions.js');
-var PythonShell = require('python-shell');
 require('dotenv').config();
 var DB = new DBFunctions();
 
@@ -13,27 +12,9 @@ amqp.connect('amqp://' + process.env.mquser + ':' + process.env.mqpassword + '@'
     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q);
     ch.consume(q, function(msg) {
       console.log(" [x] Received %s", msg.content.toString());
-      var conn = DB.connectToDBpreprocess();
-      DB.pullData(conn, msg.content).then(function(result){
-        var options = {
-          mode: 'text',
-          pythonPath: '/bin/python',
-          pythonOptions: ['-u'],
-          scriptPath: './',
-          args: [result]
-        };
-
-        PythonShell.run('dummy_processing.py', options, function (err, results) {
-        if (err) throw err;
-        // results is an array consisting of messages collected during execution
-        console.log('results: %j', results);
-        });
-      }).finally(function(){
+    }).finally(function() {
         conn.end();
         ch.ack(msg);
-      });
-      
-      
-    }, {noAck: false});
-  });
+    }); 
+  }, {noAck: false});
 });
